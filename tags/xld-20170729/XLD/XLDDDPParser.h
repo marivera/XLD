31cf6739804e8b5a90aebc78ea7655cee2aed159
tmkk <tmkk@06cd4574-f03a-dd4f-40e6-7570b67c5ae5>
2017-07-29 03:36:41 +0000

    20170729 release tag

---
+++ tags/xld-20170729/XLD/XLDDDPParser.h
@@ -0,0 +1,23 @@
+//
+//  XLDDDPParser.h
+//  XLD
+//
+//  Created by tmkk on 09/03/13.
+//  Copyright 2009 tmkk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XLDDDPParser : NSObject {
+	int offsetBytes;
+	NSString *dataFile;
+	NSString *pqDescrFile;
+	NSString *cdtextFile;
+}
+- (int)offsetBytes;
+- (int)getNumberInStr:(char *)str;
+- (NSString *)pcmFile;
+- (NSMutableArray *)trackListArray;
+- (BOOL)openDDPMS:(NSString *)path;
+@end
